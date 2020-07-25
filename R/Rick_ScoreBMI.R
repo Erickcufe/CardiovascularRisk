36f738feabd2c61c30993914951f2f1c405e9b78
Erick Cuevas <erickcufe94@gmail.com>
2020-07-25 14:45:42 -0500

    Create a function with Body Mass Index, without diabetes

--- R/Rick_ScoreBMI.R
+++ R/Rick_ScoreBMI.R
@@ -1,41 +1,39 @@
-#' Risk Score
+#' Risk Score Body Mass Index
 #'
 #' A function that return the risk level of Cardiovascular Disease in Central Latin America. Based on WHO cardiovascular disease risk.
 #'
-#' @param diabetes Binary form, non-diabetic = 0; diabetic = 1.
 #' @param gender Binary form, 0 = female; 1 = male.
 #' @param smoke Binary form, 0 = non-smoker; 1 = smoker.
 #' @param age Age in years
 #' @param sbp Systolic Blood Pressure in mmHG
-#' @param cholesterol Total cholesterol in mmol/l
+#' @param bmi Body Mass Index (kg/m^2)
 #'
 #' @return
 #' A data.frame with the Score of risk level of Cardiovascular Disease
 #'
 #'
 #' @examples
-#' Risk_Score(diabetes = 1, gender = 0, smoke = 1, age = 60,
-#                              sbp = 169, cholesterol = 8)
+#' Risk_ScoreBMI(gender = 0, smoke = 1, age = 60,
+#                              sbp = 169, BMI = 29)
 #'
 #' Age <- c(44, 57, 66, 50, 71, 37)
 #' Gender <- c(0,1,0,1,1, 1)
 #' Smoking <- c(1,1,0,1,0, 1)
 #' Systolic_Blood_Pressure <- c(120, 137, 144, 178, 163, 139)
-#' Diabetes <- c(1,1,1,0,1, 1)
-#' cholesterol <- c(0, 5.8, 6.5, 0, 7.2, 6)
-#' Risk_Score(diabetes = Diabetes, gender = Gender,smoke = Smoking, age = Age, sbp = Systolic_Blood_Pressure,cholesterol = cholesterol)
+#' BMI <- c(20,34,30,27,21, 19)
+#' Risk_ScoreBMI(gender = Gender,smoke = Smoking, age = Age, sbp = Systolic_Blood_Pressure,bmi = BMI)
 #'
-#' @rdname Risk_Score
+#' @rdname Risk_ScoreBMI
 #' @export
-Risk_Score <- function(diabetes, gender, smoke, age, sbp, cholesterol){
-  UseMethod("Risk_Score")
+Risk_ScoreBMI <- function(gender, smoke, age, sbp, bmi){
+  UseMethod("Risk_ScoreBMI")
 }
 
 #' @return \code{NULL}
 #'
-#' @rdname Risk_Score
+#' @rdname Risk_ScoreBMI
 #' @export
-Risk_Score.vector <- function(diabetes, gender, smoke, age, sbp, cholesterol){
+Risk_ScoreBMI.vector <- function(gender, smoke, age, sbp, bmi){
 
   age_levels <- function(x){
     if(x < 40){x <- 8}
@@ -61,27 +59,27 @@ Risk_Score.vector <- function(diabetes, gender, smoke, age, sbp, cholesterol){
     return(x)
   }
 
-  cho_levels <- function(x) {
-    if(x < 4){x <- 1}
-    if(x >= 4 & x <= 4.9){x <- 2}
-    if(x >= 5 & x <= 5.9){x <- 3}
-    if(x >= 6 & x <= 6.9){x <- 4}
-    if(x >= 7){x <- 5}
+  bmi_levels <- function(x) {
+    if(x < 20){x <- 1}
+    if(x >= 20 & x <= 24){x <- 2}
+    if(x >= 25 & x <= 29){x <- 3}
+    if(x >= 30 & x <= 35){x <- 4}
+    if(x >= 35){x <- 5}
 
     return(x)
   }
 
   age_1 <- sapply(age, age_levels)
   sbp_1 <- sapply(sbp, sbp_levels)
-  cholesterol_1 <- sapply(cholesterol, cho_levels)
+  bmi_1 <- sapply(bmi, bmi_levels)
 
 
-  df_dummy <- data.frame(Diabetes = diabetes, Gender = gender, Smoke = smoke,
+  df_dummy <- data.frame(Gender = gender, Smoke = smoke,
                          Age = age,
-                         SBP = sbp, Total_Cholesterol = cholesterol)
-  df <- data.frame(Diabetes = diabetes, Gender = gender, Smoke = smoke,
+                         SBP = sbp, BMI = bmi)
+  df <- data.frame(Gender = gender, Smoke = smoke,
                    Age = age_1,
-                   SBP = sbp_1, Total_Cholesterol = cholesterol_1)
+                   SBP = sbp_1, BMI = bmi_1)
 
   if(length(which(df$Age == 8)) > 0){
     thebad <- which(df$Age == 8)
@@ -93,10 +91,10 @@ Risk_Score.vector <- function(diabetes, gender, smoke, age, sbp, cholesterol){
     if(df$Age[i] == 8){
       select[i] <- "NR"
     } else {
-      dat <- CardiovascularRisk::ref1
-      select[i] <- dat[dat$Diabetes == df$Diabetes[i] & dat$Gender == df$Gender[i] &
+      dat <- CardiovascularRisk::ref2
+      select[i] <- dat[dat$Gender == df$Gender[i] &
                          dat$Smoke == df$Smoke[i] & dat$Age == df$Age[i] &
-                         dat$SBP == df$SBP[i] & dat$Cholesterol == df$Total_Cholesterol[i],
+                         dat$SBP == df$SBP[i] & dat$BMI == df$BMI[i],
                        "Score"]
     }
   }
@@ -107,10 +105,10 @@ Risk_Score.vector <- function(diabetes, gender, smoke, age, sbp, cholesterol){
 }
 
 
-#' Risk Score
+#' Risk Score Body Mass Index
 #'
 #'
-#' @param df A dataframe with columns in the following order: Diabetes (0 = non-diabetic, 1 = diabetic), Gender (0 = female, 1 = male), Smoke (0 = non-smoker, 1 = smoker), age (year) , sbp (Sytolic Blood Pressure in mmHG), cholesterol (Total cholesterol in mmol / l)
+#' @param df A dataframe with columns in the following order: Gender (0 = female, 1 = male), Smoke (0 = non-smoker, 1 = smoker), age (year) , sbp (Sytolic Blood Pressure in mmHG), BMI (Body Mass Index in kg/m^2)
 #'
 #' @return \code{NULL}
 #'
@@ -124,19 +122,18 @@ Risk_Score.vector <- function(diabetes, gender, smoke, age, sbp, cholesterol){
 #' Gender <- c(0,1,0,1,1, 1)
 #' Smoking <- c(1,1,0,1,0, 1)
 #' Systolic_Blood_Pressure <- c(120, 137, 144, 178, 163, 139)
-#' Diabetes <- c(1,1,1,0,1, 1)
-#' cholesterol <- c(0, 5.8, 6.5, 0, 7.2, 6)
-#' df <- data.frame(Diabetes, Gender, Smoking, Age, Systolic_Blood_Pressure, cholesterol)
+#' bmi <- c(20,34,30,27,21, 19)
+#' df <- data.frame(Gender, Smoking, Age, Systolic_Blood_Pressure, bmi)
 #'
-#' Risk_Score(df)
+#' Risk_ScoreBMI(df)
 #'
-#' @rdname Risk_Score
+#' @rdname Risk_ScoreBMI
 #' @export
-Risk_Score.data.frame <- function(df){
+Risk_ScoreBMI.data.frame <- function(df){
 
-  colnames(df) <- c("Diabetes", "Gender", "Smoke", "Age", "SBP", "Total_Cholesterol")
+  colnames(df) <- c("Gender", "Smoke", "Age", "SBP", "BMI")
   df_old <- df
-  colnames(df_old) <- c("Diabetes", "Gender", "Smoke", "Age", "SBP", "Total_Cholesterol")
+  colnames(df_old) <- c("Gender", "Smoke", "Age", "SBP", "BMI")
   age_levels <- function(x){
     if(x < 40){x <- 8}
     if(x > 75){x <- 8}
@@ -161,19 +158,19 @@ Risk_Score.data.frame <- function(df){
     return(x)
   }
 
-  cho_levels <- function(x) {
-    if(x < 4){x <- 1}
-    if(x >= 4 & x <= 4.9){x <- 2}
-    if(x >= 5 & x <= 5.9){x <- 3}
-    if(x >= 6 & x <= 6.9){x <- 4}
-    if(x >= 7){x <- 5}
+  bmi_levels <- function(x) {
+    if(x < 20){x <- 1}
+    if(x >= 20 & x <= 24){x <- 2}
+    if(x >= 25 & x <= 29){x <- 3}
+    if(x >= 30 & x <= 35){x <- 4}
+    if(x >= 35){x <- 5}
 
     return(x)
   }
 
   df$Age <- sapply(df$Age, age_levels)
   df$SBP <- sapply(df$SBP, sbp_levels)
-  df$Total_Cholesterol <- sapply(df$Total_Cholesterol, cho_levels)
+  df$BMI <- sapply(df$BMI, bmi_levels)
 
   if(length(which(df$Age == 8)) > 0){
     thebad <- which(df$Age == 8)
@@ -185,10 +182,10 @@ Risk_Score.data.frame <- function(df){
     if(df$Age[i] == 8){
       select[i] <- "NR"
     } else {
-      dat <- CardiovascularRisk::ref1
-      select[i] <- dat[dat$Diabetes == df$Diabetes[i] & dat$Gender == df$Gender[i] &
+      dat <- CardiovascularRisk::ref2
+      select[i] <- dat[dat$Gender == df$Gender[i] &
                          dat$Smoke == df$Smoke[i] & dat$Age == df$Age[i] &
-                         dat$SBP == df$SBP[i] & dat$Cholesterol == df$Total_Cholesterol[i],
+                         dat$SBP == df$SBP[i] & dat$BMI == df$BMI[i],
                        "Score"]
     }
   }
